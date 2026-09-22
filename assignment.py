155fb403840064258e73544a06ba52159955079c
Eme112 <eme_pedraza@hotmail.com>
2026-09-23 00:35:36 +0200

    Added comments to assignment.py

--- assignment.py
+++ assignment.py
@@ -1,6 +1,6 @@
 # Assign string to variable
-name = "Ada"
-first_letter = name[0]
-second_letter = name[1]
-print(first_letter)
-print(second_letter)
+name = "Ada" # Creates a variable
+first_letter = name[0] # Assigns the first letter of the string to a variable
+second_letter = name[1] # Assigns the second letter of the string to a variable
+print(first_letter) # Prints the first letter
+print(second_letter) # Prints the second letter
